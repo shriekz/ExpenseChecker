92bd9c3551d073849fe0fa4aa444bf34ddd4f66e
Srikanth <srikanth.swaminathan@investisdigital.com>
2022-02-15 06:22:58 +0530

    first

---
+++ msgprocessor.py
@@ -0,0 +1,67 @@
+from datetime import datetime
+import referencedata as refd
+import pandas as pd
+import numpy as np
+
+
+# load xml to dataframe
+def getAcctNumIfAny(smstext):
+    acct_num = [acct for acct in refd.acct_dict if acct in smstext]
+
+    return acct_num[0] if len(acct_num) else ''
+
+
+def loadXMLtoDF():
+    cols_to_use = ['address', 'body', 'readable_date', 'contact_name']
+    df = pd.read_xml(refd.filename, xpath=".//sms")
+    df1 = df[cols_to_use].copy()
+    df1['category'] = 'uncategorized'
+    df1['account_num'] = 'X'
+    df1['month'] = 0
+    df1['year'] = 0
+    df1['Merchant'] = 'M'
+    df1['IsDebit'] = True
+    df1['Amount'] = 0.0
+
+    for idx, sms in df1.iterrows():
+        date_time_obj = datetime.strptime(sms['readable_date'], '%b %d, %Y %I:%M:%S %p')
+        acct_num = getAcctNumIfAny(sms['body'])
+        df1.at[idx, 'account_num'] = acct_num
+        df1.at[idx, 'month'] = date_time_obj.month
+        df1.at[idx, 'year'] = date_time_obj.year
+        if df1.at[idx, 'account_num']:
+            # get a function pointer for each account number that holds the
+            # function that is used to process the string for the account.
+            fp = getattr(refd, refd.acct_dict[acct_num])
+
+            # Interesting way of calling a function using the function pointer
+            # it returns multiple values.
+            isProcess, isDebit, Amt, Category = fp(sms['body'], sms['address'])
+            if isProcess:
+                df1.at[idx, 'IsDebit'], df1.at[idx, 'Amount'] = isDebit, Amt
+                df1.at[idx, 'category'] = Category
+
+    return df1
+
+
+# process all sms data
+def aggregateStatistics(df):
+    # group by all values by account num, year, month and get sum
+    gp = df.groupby(['account_num', 'IsDebit', 'year', 'month', 'category']).agg(np.sum)
+    gp.to_csv('temp.csv')
+
+
+def processAccount():
+    #   Load the XML to DF
+    df = loadXMLtoDF()
+
+    #   Filter DF for acct_number add acct_number to DF col
+    aggregateStatistics(df)
+
+    # Dump df to file
+    df.to_csv('temp1.csv')
+    #   Get date and amount and load them into DF
+
+
+if __name__ == '__main__':
+    processAccount()
---
+++ referencedata.py
@@ -0,0 +1,104 @@
+import re
+
+filename = 'XMLSource/sms.xml'
+acct_dict = {
+    'XX1023': 'processAccount1023',
+    'XX657': 'processAccount657'
+}
+reject_list = ['declined']
+
+category_mapping_list = {
+    'apollo': 'Healthcare',
+    'isabel': 'Healthcare',
+    'kavery medi': 'Healthcare',
+    'pharma': 'Healthcare',
+    'swiggy': 'Food',
+    'sangeetha veg': 'Food',
+    'foods': 'Groceries',
+    'pazhamu': 'Groceries',
+    'agoda': 'Travel',
+    'hpcl': 'Fuel',
+    'bpcl': 'Fuel',
+    'iocl': 'Fuel',
+    'jayam enter': 'Groceries',
+    'spencers': 'Groceries',
+    'sri krishna': 'Food',
+    'ice crea': 'Food',
+    'ach*indian': 'Investment',
+    'bd-mf': 'Investment',
+    'school': 'Education',
+    'cinema': 'Entertainment',
+    'hathway': 'Utilities',
+    'tangedco': 'Utilities',
+    'tneb': 'Utilities',
+    'bil*onl*00032': 'Utilities',
+    'bigbasket': 'Groceries',
+    'hicare': 'Utilities',
+    'jayaraj': 'Fitness',
+    'pizza': 'Food',
+    'ramanathan': 'Kids Activities',
+    'shanmugam': 'Kids Activities',
+    'zerodha': 'Investment',
+    'amrutha sri': 'Groceries',
+    'aarthi scans': 'Heathcare',
+    'netmeds': 'Healthcare',
+    'petrol': 'Fuel',
+    'ayurved': 'Healthcare',
+    'mcrenet': 'Food'
+}
+
+
+def processAccount1023(text, address):
+    bank = 'icici'
+    category = 'Unknown'
+    if bank not in address.lower():
+        return False, False, 0.0, 'Uncategorized'
+
+    text = text.replace(',', '')
+    regex = '(?:INR )(\\d+\\.\\d+)(?: is debited)'
+    r1 = '(?:Info: )(.*?)(?:\\.)'
+    matches = re.search(regex, text)
+    if matches:
+        Amt = float(matches.group(1))
+        m = re.search(r1, text)
+        if m:
+            merchant = m.group(1).strip().lower()
+            for key in category_mapping_list:
+                if key in merchant:
+                    category = category_mapping_list[key]
+                    break
+
+        return True, True, Amt, category
+    else:
+        return False, False, 0.0, 'Uncategorized'
+
+
+def processAccount657(text, address):
+
+    bank = 'icici'
+    category = 'Unknown'
+    if bank not in address.lower():
+        return False, False, 0.0, 'Uncategorized'
+
+    # remove all "," chars in string as don't know to process it.
+    text = text.replace(',', '')
+    regex = '((Acc(oun)?t) (?:XX|XXX)657.*)(debited.*)(INR *|Rs )(\\d+(\\.\\d+)?)( on)'
+    r1 = '(?:Info: )(.*?)(?:\\.)'
+    r2 = '(?:debited.*(?:;|&))(.*)(credited\\.)'
+    category = 'Unknown'
+    matches = re.search(regex, text)
+    if matches:
+        Amt = float(matches.group(6))
+        m = re.search(r1, text)
+        if m is None:
+            m = re.search(r2, text)
+        if m:
+            merchant = m.group(1).strip().lower().lstrip('&')
+            for key in category_mapping_list:
+                if key in merchant:
+                    category = category_mapping_list[key]
+                    break
+
+        return True, True, Amt, category
+    else:
+        return False, False, 0.0, category
